9015707d18370ac2fe4300bc355f56d1821f945e
산체스 <xognsl87@gmail.com>
2022-01-05 10:59:02 +0900

    feat: Network Request에 사용할 Requestable 구현

---
+++ SanchesMarket/Protocol/RequestProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  Requestable.swift
+//  SanchesMarket
+//
+//  Created by tae hoon park on 2022/01/05.
+//
+
+import Foundation
+
+protocol Requestable {
+    var url: APIURL { get }
+    var httpMethod: APIHTTPMethod { get }
+    var contentType: ContentType { get }
+}
