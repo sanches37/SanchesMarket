60d1d57e3d924737b2812c3952a561045b8c7696
산체스 <xognsl87@gmail.com>
2022-01-05 10:53:17 +0900

    feat Network Request에 사용할 Enum 타입들 구현 APIHTTPMethod 구현 APIURL enum 구현 ContentType enum 구현 MimeType enum 구현 Boundary enum 구현

---
+++ SanchesMarket/Enumeration/RequestEnum.swift
@@ -0,0 +1,83 @@
+//
+//  RequestEnum.swift
+//  SanchesMarket
+//
+//  Created by tae hoon park on 2022/01/05.
+//
+
+import Foundation
+
+enum APIHTTPMethod {
+    case get
+    case post
+    case patch
+    case delete
+    
+    var value: String {
+        switch self {
+        case .delete:
+            return "GET"
+        case .get:
+            return "POST"
+        case .patch:
+            return "PATCH"
+        case .post:
+            return "DELETE"
+        }
+    }
+}
+
+enum APIURL {
+    case getItems(page: Int)
+    case getItem(id: Int)
+    case post
+    case patch(id: Int)
+    case delete(id: Int)
+    
+    private static let baseURL = "https://camp-open-market-2.herokuapp.com"
+    
+    var path: String {
+        switch self {
+        case .getItems(let page):
+            return Self.baseURL + "items/\(page)"
+        case .getItem(let id), .patch(let id), .delete(let id):
+            return Self.baseURL + "item/\(id)"
+        case .post:
+            return Self.baseURL + "item/"
+        }
+    }
+}
+
+enum ContentType {
+    case json
+    case multipart
+    
+    static let httpHeaderField = "Content-Type"
+    
+    var format: String {
+        switch self {
+        case .json:
+            return "application/json"
+        case .multipart:
+            return "multipart/form-data; boundary=\(Boundary.uuid)"
+        }
+    }
+}
+
+enum MimeType: CustomStringConvertible {
+    case jpeg
+    case png
+    
+    var description: String {
+        switch self {
+        case .jpeg:
+            return "image/jpeg"
+        case .png:
+            return "image/png"
+        }
+    }
+}
+
+enum Boundary {
+    static let uuid = "Boundary-\(UUID().uuidString)"
+}
