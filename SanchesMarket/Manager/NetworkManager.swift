b1f728993b412462c1dfeec3807851ce33360ee6
산체스 <xognsl87@gmail.com>
2022-01-06 00:30:35 +0900

    refactor: NetworkManager 타입 코드 누락되어 추가

--- SanchesMarket/Manager/NetworkManager.swift
+++ SanchesMarket/Manager/NetworkManager.swift
@@ -7,35 +7,37 @@
 
 import Foundation
 
-private let request = Request()
-private let rangeOfSuccessState = 200...299
-
-func commuteWithAPI(api: Requestable,
-                    completionHandler: @escaping(Result<Data, NetworkError>) -> Void) {
-    guard let request = try? request.createRequest(api: api) else {
-        completionHandler(.failure(.requestFailed))
-        return
-    }
+struct NetworkManager {
+    private let request = Request()
+    private let rangeOfSuccessState = 200...299
     
-    URLSession.shared.dataTask(with: request) { data, response, error in
-        if let error = error {
-            completionHandler(.failure(.unknown(description: error.localizedDescription)))
+    func commuteWithAPI(api: Requestable,
+                        completionHandler: @escaping(Result<Data, NetworkError>) -> Void) {
+        guard let request = try? request.createRequest(api: api) else {
+            completionHandler(.failure(.requestFailed))
             return
         }
-        guard let response = response as? HTTPURLResponse else {
-            completionHandler(.failure(.responseFailed))
-            return
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completionHandler(.failure(.unknown(description: error.localizedDescription)))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completionHandler(.failure(.responseFailed))
+                return
+            }
+            guard rangeOfSuccessState.contains(response.statusCode) else {
+                completionHandler(.failure(.outOfRange(statusCode: response.statusCode)))
+                return
+            }
+            guard let data = data else {
+                completionHandler(.failure(.dataNotfound))
+                return
+            }
+            completionHandler(.success(data))
         }
-        guard rangeOfSuccessState.contains(response.statusCode) else {
-            completionHandler(.failure(.outOfRange(statusCode: response.statusCode)))
-            return
-        }
-        guard let data = data else {
-            completionHandler(.failure(.dataNotfound))
-            return
-        }
-        completionHandler(.success(data))
+        .resume()
     }
-    .resume()
 }
 
