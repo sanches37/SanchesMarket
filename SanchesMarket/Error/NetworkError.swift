3da515ae2336fb58fe761afddf3994f4987bc8e7
산체스 <xognsl87@gmail.com>
2022-01-06 23:24:41 +0900

    refactor: NetworkError errorDescription 추가

--- SanchesMarket/Error/NetworkError.swift
+++ SanchesMarket/Error/NetworkError.swift
@@ -14,4 +14,21 @@ enum NetworkError: Error, LocalizedError {
     case responseFailed
     case outOfRange(statusCode: Int)
     case dataNotfound
+    
+    var errorDescription: String {
+        switch self {
+        case .invalidURL:
+            return "잘못된 URL입니다."
+        case .requestFailed:
+            return "리퀘스트를 받는데 실패했습니다."
+        case .unknown(let description):
+            return "에러: \(description)"
+        case .responseFailed:
+            return "리스폰스를 받는데 실패했습니다."
+        case .outOfRange(let statusCode):
+            return "상태코드: \(statusCode)"
+        case .dataNotfound:
+            return "데이터를 전달 받지 못했습니다."
+        }
+    }
 }
