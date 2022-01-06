9bbc63ba4d989600a04a524d93659c9df0ca0755
산체스 <xognsl87@gmail.com>
2022-01-06 23:36:55 +0900

    refactor: NetworkManager 의존성주입할 수 있도록 수정 테스트를 위해 init 에 session, applicableHTTPMethod 를다르게적용할 수 있게 구현 request 테스트를 할 수 있도록 APIHTTPMethod 타입에 caseCaseIterable 채택 request 테스트를 할 수 있도록 APIHTTPMethod case를 주입할 수 있도록 수정

--- SanchesMarket/Enumeration/RequestEnum.swift
+++ SanchesMarket/Enumeration/RequestEnum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum APIHTTPMethod {
+enum APIHTTPMethod: CaseIterable {
     case get
     case post
     case patch
--- SanchesMarket/Manager/NetworkManager.swift
+++ SanchesMarket/Manager/NetworkManager.swift
@@ -9,7 +9,14 @@ import Foundation
 
 struct NetworkManager {
     private let request = Request()
-    private let rangeOfSuccessState = 200...299
+    private let session: URLSessionProtocol
+    private var applicableHTTPMethod: [APIHTTPMethod]
+    private static let rangeOfSuccessState = 200...299
+    
+    init(session: URLSessionProtocol = URLSession.shared, applicableHTTPMethod: [APIHTTPMethod] = APIHTTPMethod.allCases) {
+        self.session = session
+        self.applicableHTTPMethod = applicableHTTPMethod
+    }
     
     func commuteWithAPI(api: Requestable,
                         completionHandler: @escaping(Result<Data, NetworkError>) -> Void) {
@@ -17,8 +24,12 @@ struct NetworkManager {
             completionHandler(.failure(.requestFailed))
             return
         }
+        guard applicableHTTPMethod.contains(api.httpMethod) else {
+            completionHandler(.failure(.responseFailed))
+            return
+        }
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        session.dataTask(with: request) { data, response, error in
             if let error = error {
                 completionHandler(.failure(.unknown(description: error.localizedDescription)))
                 return
@@ -27,7 +38,7 @@ struct NetworkManager {
                 completionHandler(.failure(.responseFailed))
                 return
             }
-            guard rangeOfSuccessState.contains(response.statusCode) else {
+            guard Self.rangeOfSuccessState.contains(response.statusCode) else {
                 completionHandler(.failure(.outOfRange(statusCode: response.statusCode)))
                 return
             }
