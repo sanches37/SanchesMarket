386296273eee1bc3a009a6febb0cfa9ddd94feba
산체스 <xognsl87@gmail.com>
2022-01-14 12:51:21 +0900

    feat: ProductListCell.xib 구현

---
+++ SanchesMarket/View/ProductCell/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  SanchesMarket
+//
+//  Created by tae hoon park on 2022/01/14.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
