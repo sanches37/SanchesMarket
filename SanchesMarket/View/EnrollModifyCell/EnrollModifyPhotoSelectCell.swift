794f425648b727bb5ae41915a4230f27ec323fab
산체스 <xognsl87@gmail.com>
2022-01-19 22:01:26 +0900

    feat: EnrollModifyPhotoSelectCell 구현 버튼 클릭시 사진선택화면으로 이동할 EnrollModifyPhotoSelectCell 구현

---
+++ SanchesMarket/View/EnrollModifyCell/EnrollModifyPhotoSelectCell.swift
@@ -0,0 +1,43 @@
+//
+//  EnrollModifyPhotoSelectCell.swift
+//  SanchesMarket
+//
+//  Created by tae hoon park on 2022/01/19.
+//
+
+import UIKit
+
+class EnrollModifyPhotoSelectCell: UICollectionViewCell {
+    static let identifier = String(describing: EnrollModifyPhotoSelectCell.self)
+    private let cameraImage = UIImage(systemName: "camera")
+    private let photoTotalNumber = 5
+    private var photoSelectNumber = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setup() {
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func configure(photoSelectButton: UIButton) {
+        photoSelectButton.setTitle(
+            "\(photoTotalNumber)/\(photoTotalNumber)", for: .normal)
+        photoSelectButton.setImage(cameraImage, for: .normal)
+        photoSelectButton.setTitleColor(.black, for: .normal)
+        photoSelectButton.tintColor = .black
+        contentView.addSubview(photoSelectButton)
+        photoSelectButton.frame = CGRect(x: 0, y: 0,
+                                         width: contentView.frame.width,
+                                         height: contentView.frame.height)
+    }
+}
